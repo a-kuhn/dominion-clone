688b73570dca74d8634cd6df16b7f8f4e3a82f84
alex k <alex.kuhn1123@gmail.com>
2020-04-29 14:09:25 -0700

    Workshop.Play() updated, still incomplete

--- Models/Cards/Action/Workshop.cs
+++ Models/Cards/Action/Workshop.cs
@@ -8,7 +8,13 @@ namespace DominionClone.Models
     public class Workshop : Card
     {
         //gain 1 card costing up to 4 Treasure
-
+        public override void Play(Player player)
+        {
+            //need to filter Game's cards
+            // c --> c.Cost <= 4
+            // player can select any card from filtered list
+            // player.Buy(selectedCard);
+        }
         public Workshop(string type = "Action", string title = "Workshop", int cost = 3) : base(type, title, cost) { }
     }
 }
