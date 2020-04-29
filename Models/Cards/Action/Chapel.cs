aa5b82a6132ddee0bbc97d6fd951fb0ecf6be7b2
alex k <alex.kuhn1123@gmail.com>
2020-04-29 13:01:02 -0700

    Chapel.Play() started, not completed

--- Models/Cards/Action/Chapel.cs
+++ Models/Cards/Action/Chapel.cs
@@ -8,14 +8,26 @@ namespace DominionClone.Models
     public class Chapel : Card
     {
         // trash up to 4 cards from your hand
-        public void Play()
+        public override void Play(Player player)
         {
+            //keep track of # of cards to trash
             int trashCount = 0;
+
+            //keep track of which cards in player's hand
+            List<int> trashIdx = new List<int>();
+
+            //allow player to select up to 4 cards to trash
             while (trashCount <= 4)
             {
                 //player can select card from hand to trash (trashCount ++)
                 //OR player can submit 
                 //on submit, selected cards are trashed (remove from player's deck, add to game's trash)
+            }
+
+            //trash selected cards
+            foreach (int idx in trashIdx)
+            {
+                player.Trash(idx);
             }
         }
 
